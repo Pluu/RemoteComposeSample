efb061bb94d035cb2e939a1481b130c40cb8b346
pluu <pluulove@gmail.com>
2026-07-31 21:43:22 +0900

    Update minSdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.pluu.sample.remote.compose"
-        minSdk = 28
+        minSdk = 29
         targetSdk = 37
         versionCode = 1
         versionName = "1.0"
