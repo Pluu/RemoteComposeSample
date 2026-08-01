a3f9a6c3e911866d94c37aa7481121a3958e3a68
pluu <pluulove@gmail.com>
2026-08-01 22:06:24 +0900

    Cleanup

--- server/src/main/kotlin/com/pluu/sample/remote/server/ServerMain.kt
+++ server/src/main/kotlin/com/pluu/sample/remote/server/ServerMain.kt
@@ -1,11 +1,5 @@
 package com.pluu.sample.remote.server
 
-import com.pluu.sample.remote.server.ui.customApi
-import com.pluu.sample.remote.server.ui.itemsApi
-import com.pluu.sample.remote.server.ui.menuApi
-import com.pluu.sample.remote.server.ui.menuSampleApi
-import com.pluu.sample.remote.server.ui.remoteApi
-import com.pluu.sample.remote.server.ui.schemesApi
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.install
 import io.ktor.server.engine.embeddedServer
