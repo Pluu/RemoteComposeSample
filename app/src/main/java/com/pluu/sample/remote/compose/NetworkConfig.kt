05962c97202b59d24754c7cad7dd9baf5e602d90
pluu <pluulove@gmail.com>
2026-08-01 13:02:33 +0900

    에뮬레이터 자동 적용

--- app/src/main/java/com/pluu/sample/remote/compose/NetworkConfig.kt
+++ app/src/main/java/com/pluu/sample/remote/compose/NetworkConfig.kt
@@ -2,9 +2,9 @@ package com.pluu.sample.remote.compose
 
 object NetworkConfig {
     /**
-     * Android Emulator's default gateway address to the host machine.
+     * Use localhost with adb reverse tcp:8080 tcp:8080.
      */
-    private const val EMULATOR_HOST = "10.0.2.2"
+    private const val EMULATOR_HOST = "127.0.0.1"
     private const val PORT = 8080
     
     const val BASE_URL = "http://$EMULATOR_HOST:$PORT"
