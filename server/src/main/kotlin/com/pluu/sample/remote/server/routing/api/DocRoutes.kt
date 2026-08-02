d033c3c6d59409ea0c842b2b1e793762f5178fa0
pluu <pluulove@gmail.com>
2026-08-02 16:37:52 +0900

    Feat: API Sample 리스트를 가로 스크롤 탭 구조로 변경 및 상세 페이지 통합

--- server/src/main/kotlin/com/pluu/sample/remote/server/routing/api/DocRoutes.kt
+++ server/src/main/kotlin/com/pluu/sample/remote/server/routing/api/DocRoutes.kt
@@ -2,25 +2,16 @@ package com.pluu.sample.remote.server.routing.api
 
 import androidx.compose.remote.creation.dsl.Modifier
 import androidx.compose.remote.creation.dsl.RcFontWeight
-import androidx.compose.remote.creation.dsl.RcHorizontalPositioning
-import androidx.compose.remote.creation.dsl.RcPaintStyle
 import androidx.compose.remote.creation.dsl.RcProfile
 import androidx.compose.remote.creation.dsl.RcRowHorizontalPositioning
 import androidx.compose.remote.creation.dsl.RcVerticalPositioning
-import androidx.compose.remote.creation.dsl.background
 import androidx.compose.remote.creation.dsl.createRcBuffer
 import androidx.compose.remote.creation.dsl.fillMaxSize
 import androidx.compose.remote.creation.dsl.fillMaxWidth
-import androidx.compose.remote.creation.dsl.graphicsLayer
-import androidx.compose.remote.creation.dsl.height
 import androidx.compose.remote.creation.dsl.heightIn
 import androidx.compose.remote.creation.dsl.onClick
-import androidx.compose.remote.creation.dsl.onDoubleClick
-import androidx.compose.remote.creation.dsl.onLongClick
 import androidx.compose.remote.creation.dsl.padding
-import androidx.compose.remote.creation.dsl.rdp
 import androidx.compose.remote.creation.dsl.rsp
-import androidx.compose.remote.creation.dsl.size
 import com.pluu.sample.remote.server.utils.getHeaderTags
 import io.ktor.server.response.respondBytes
 import io.ktor.server.routing.Route
@@ -75,203 +66,7 @@ fun Route.docRoutes(rcProfile: RcProfile) {
                         }
 
                         // Content
-                        Column(
-                            Modifier
-                                .fillMaxWidth()
-                                .padding(top = 16f),
-                        ) {
-                            when (name) {
-                                "Text" -> {
-                                    Text("Normal Text", fontSize = 16.rsp)
-                                    Text("Bold Text", fontSize = 20.rsp, fontWeight = RcFontWeight.Bold)
-                                    Text("Large Text", fontSize = 32.rsp)
-                                    Text("Custom Color", color = 0xFFFF0000.toInt())
-                                }
-
-                                "Button" -> {
-                                    Box(
-                                        modifier =
-                                            Modifier
-                                                .fillMaxWidth()
-                                                .height(48.rdp)
-                                                .background(0xFF6200EE)
-                                                .onClick {
-                                                    hostAction("{\"action\":\"toast\",\"message\":\"Button Clicked!\"}")
-                                                },
-                                        vertical = RcVerticalPositioning.Center,
-                                        horizontal = RcHorizontalPositioning.Center,
-                                    ) {
-                                        Text("Click Me", color = 0xFFFFFFFF.toInt())
-                                    }
-                                }
-
-                                "Modifier" -> {
-                                    Box(Modifier.size(100.rdp).background(0xFFFF0000.toInt()))
-                                    Box(Modifier.padding(top = 10f).size(100.rdp).background(0xFF00FF00.toInt()))
-                                    Box(
-                                        Modifier
-                                            .padding(top = 10f)
-                                            .fillMaxWidth()
-                                            .height(50.rdp)
-                                            .background(0xFF0000FF.toInt()),
-                                    )
-                                }
-
-                                "Layout" -> {
-                                    Text("Column", fontWeight = RcFontWeight.Bold)
-                                    Column(Modifier.background(0xFFEEEEEE.toInt()).padding(8f)) {
-                                        Text("Item 1")
-                                        Text("Item 2")
-                                    }
-                                    Text("Row", Modifier.padding(top = 16f), fontWeight = RcFontWeight.Bold)
-                                    Row(Modifier.background(0xFFDDDDDD.toInt()).padding(8f)) {
-                                        Text("Left ")
-                                        Text("Right")
-                                    }
-                                }
-
-                                "Icon", "Image" -> {
-                                    Text("Image from URL")
-                                    Image(
-                                        image =
-                                            remoteBitmapUrl(
-                                                "https://raw.githubusercontent.com/pluu/RemoteComposeSample/master/doc/img/sample.png",
-                                            ),
-                                        modifier = Modifier.size(200.rdp),
-                                    )
-                                }
-
-                                "Checkbox", "Switch" -> {
-                                    Row(vertical = RcVerticalPositioning.Center) {
-                                        Box(
-                                            modifier =
-                                                Modifier
-                                                    .size(24.rdp)
-                                                    .background(0xFFCCCCCC.toInt())
-                                                    .onClick {
-                                                        // Simulation: toggle logic would need expressions or host-side state
-                                                        hostAction("{\"action\":\"toast\",\"message\":\"Toggled!\"}")
-                                                    },
-                                        )
-                                        Text("  Toggle Component Sample", Modifier.padding(start = 8f))
-                                    }
-                                }
-
-                                "RcScope" -> {
-                                    Global {
-                                        Box(
-                                            Modifier
-                                                .fillMaxWidth()
-                                                .height(100.rdp)
-                                                .background(0xFFEEEEEE.toInt()),
-                                            vertical = RcVerticalPositioning.Center,
-                                            horizontal = RcHorizontalPositioning.Center,
-                                        ) {
-                                            Text("Inside Global and Box nesting", fontSize = 18.rsp)
-                                        }
-                                    }
-                                }
-
-                                "RcTypes" -> {
-                                    val f = remoteFloat(3.14159f)
-                                    val c = remoteColor(0xFFFF0000.toInt())
-
-                                    Text("Remote Integer (Constant): 42")
-                                    Text("Remote Float: " + f.format(1, 4))
-                                    Row(vertical = RcVerticalPositioning.Center) {
-                                        Text("Remote Color: ")
-                                        Box(Modifier.size(24.rdp).background(c))
-                                    }
-                                }
-
-                                "DrawScope", "RcDrawing" -> {
-                                    Text("Canvas Drawing Example", fontWeight = RcFontWeight.Bold)
-                                    Canvas(Modifier.size(200.rdp).background(0xFFF0F0F0.toInt())) {
-                                        paint {
-                                            color(0xFFFF0000.toInt())
-                                        }
-                                        drawRect(10f, 10f, 90f, 90f)
-
-                                        paint {
-                                            color(0xFF0000FF.toInt())
-                                        }
-                                        drawCircle(150f, 150f, 40f)
-
-                                        paint {
-                                            color(0xFF00FF00.toInt())
-                                            strokeWidth(5f)
-                                            style(RcPaintStyle.Stroke)
-                                        }
-                                        drawLine(10f, 150f, 100f, 190f)
-                                    }
-                                }
-
-                                "RcInteractivity", "Gestures" -> {
-                                    Column(Modifier.fillMaxWidth().padding(8f)) {
-                                        Box(
-                                            Modifier
-                                                .fillMaxWidth()
-                                                .height(60.rdp)
-                                                .background(0xFFE0E0E0.toInt())
-                                                .onClick {
-                                                    hostAction("{\"action\":\"toast\",\"message\":\"Single Click!\"}")
-                                                },
-                                            vertical = RcVerticalPositioning.Center,
-                                            horizontal = RcHorizontalPositioning.Center,
-                                        ) {
-                                            Text("Single Click")
-                                        }
-
-                                        Box(
-                                            Modifier
-                                                .padding(top = 8f)
-                                                .fillMaxWidth()
-                                                .height(60.rdp)
-                                                .background(0xFFD0D0D0.toInt())
-                                                .onLongClick {
-                                                    hostAction("{\"action\":\"toast\",\"message\":\"Long Click!\"}")
-                                                },
-                                            vertical = RcVerticalPositioning.Center,
-                                            horizontal = RcHorizontalPositioning.Center,
-                                        ) {
-                                            Text("Long Click")
-                                        }
-
-                                        Box(
-                                            Modifier
-                                                .padding(top = 8f)
-                                                .fillMaxWidth()
-                                                .height(60.rdp)
-                                                .background(0xFFC0C0C0.toInt())
-                                                .onDoubleClick {
-                                                    hostAction("{\"action\":\"toast\",\"message\":\"Double Click!\"}")
-                                                },
-                                            vertical = RcVerticalPositioning.Center,
-                                            horizontal = RcHorizontalPositioning.Center,
-                                        ) {
-                                            Text("Double Click")
-                                        }
-                                    }
-                                }
-
-                                "Animation" -> {
-                                    val time = animationTime()
-                                    Text("Animation using animationTime()")
-                                    Box(
-                                        Modifier
-                                            .padding(top = 16f)
-                                            .size(100.rdp)
-                                            .graphicsLayer {
-                                                rotationZ(time * 45f) // Rotate 45 degrees per second
-                                            }.background(0xFF6200EE.toInt()),
-                                    )
-                                }
-
-                                else -> {
-                                    Text("Detail implementation for $name coming soon...", fontSize = 16.rsp)
-                                }
-                            }
-                        }
+                        renderSampleContent(name)
                     }
                 }
             call.respondBytes(bytes)
--- server/src/main/kotlin/com/pluu/sample/remote/server/routing/ui/ApiListUiRoutes.kt
+++ server/src/main/kotlin/com/pluu/sample/remote/server/routing/ui/ApiListUiRoutes.kt
@@ -5,13 +5,19 @@ import androidx.compose.remote.creation.dsl.RcFontWeight
 import androidx.compose.remote.creation.dsl.RcProfile
 import androidx.compose.remote.creation.dsl.RcRowHorizontalPositioning
 import androidx.compose.remote.creation.dsl.RcVerticalPositioning
+import androidx.compose.remote.creation.dsl.background
 import androidx.compose.remote.creation.dsl.createRcBuffer
 import androidx.compose.remote.creation.dsl.fillMaxSize
 import androidx.compose.remote.creation.dsl.fillMaxWidth
+import androidx.compose.remote.creation.dsl.height
 import androidx.compose.remote.creation.dsl.heightIn
+import androidx.compose.remote.creation.dsl.horizontalScroll
 import androidx.compose.remote.creation.dsl.onClick
 import androidx.compose.remote.creation.dsl.padding
+import androidx.compose.remote.creation.dsl.rdp
 import androidx.compose.remote.creation.dsl.rsp
+import androidx.compose.remote.creation.dsl.size
+import com.pluu.sample.remote.server.routing.api.renderSampleContent
 import com.pluu.sample.remote.server.utils.getHeaderTags
 import io.ktor.server.response.respondBytes
 import io.ktor.server.routing.Route
@@ -21,26 +27,29 @@ fun Route.apiListUiRoutes(rcProfile: RcProfile) {
     get("/ui/api_list") {
         val samples =
             listOf(
-                "Modifier" to "/api/doc/Modifier",
-                "RcScope" to "/api/doc/RcScope",
-                "RcTypes" to "/api/doc/RcTypes",
-                "Layout" to "/api/doc/Layout",
-                "DrawScope" to "/api/doc/DrawScope",
-                "Text" to "/api/doc/Text",
-                "Button" to "/api/doc/Button",
-                "Image" to "/api/doc/Image",
-                "Icon" to "/api/doc/Icon",
-                "Checkbox" to "/api/doc/Checkbox",
-                "Switch" to "/api/doc/Switch",
-                "RcDrawing" to "/api/doc/RcDrawing",
-                "RcInteractivity" to "/api/doc/RcInteractivity",
-                "Animation" to "/api/doc/Animation",
-                "Gestures" to "/api/doc/Gestures",
+                "Modifier",
+                "RcScope",
+                "RcTypes",
+                "Layout",
+                "DrawScope",
+                "Text",
+                "Button",
+                "Image",
+                "Icon",
+                "Checkbox",
+                "Switch",
+                "RcDrawing",
+                "RcInteractivity",
+                "Animation",
+                "Gestures",
             )
+
+        val selectedSample = call.parameters["selected"] ?: samples.first()
 
         val bytes =
             createRcBuffer(rcProfile, *getHeaderTags(call)) {
                 Column(Modifier.fillMaxSize().padding(16f)) {
+                    // Header
                     Row(
                         modifier = Modifier.fillMaxWidth().heightIn(min = 48f),
                         horizontal = RcRowHorizontalPositioning.Start,
@@ -63,21 +72,45 @@ fun Route.apiListUiRoutes(rcProfile: RcProfile) {
                         )
                     }
 
-                    samples.forEach { (name, path) ->
-                        Box(
+                    // Horizontal Tab Bar
+                    Row(
+                        modifier =
                             Modifier
                                 .fillMaxWidth()
-                                .padding(0f, 12f, 0f, 12f)
-                                .onClick {
-                                    hostAction(path)
-                                },
-                        ) {
-                            Text(
-                                text = name,
-                                fontSize = 24.rsp,
-                            )
+                                .padding(top = 16f, bottom = 8f)
+                                .horizontalScroll(),
+                        horizontal = RcRowHorizontalPositioning.Start,
+                    ) {
+                        samples.forEach { name ->
+                            val isSelected = name == selectedSample
+                            Column(
+                                modifier =
+                                    Modifier
+                                        .padding(end = 16f)
+                                        .onClick {
+                                            hostAction("/ui/api_list?selected=$name")
+                                        },
+                                horizontal = androidx.compose.remote.creation.dsl.RcHorizontalPositioning.Center,
+                            ) {
+                                Text(
+                                    text = name,
+                                    fontSize = 18.rsp,
+                                    fontWeight = if (isSelected) RcFontWeight.Bold else RcFontWeight.Normal,
+                                )
+                                if (isSelected) {
+                                    Box(
+                                        Modifier
+                                            .padding(top = 4f)
+                                            .size(width = 24.rdp, height = 2.rdp)
+                                            .background(0xFF6200EE.toInt()),
+                                    )
+                                }
+                            }
                         }
                     }
+
+                    // Selected Sample Content
+                    renderSampleContent(selectedSample)
                 }
             }
         call.respondBytes(bytes)
